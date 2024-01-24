c12a1aee5d1684cc8090b78086787b012f056bcf
cngwane <cngwane@CollinsBitsPC>
2024-01-24 05:12:58 -0500

    modified app.js and index.html

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 function randomRGB () {
-    const r = Math(Math.random() * 256)
-    const g = Math(Math.random() * 256)
-    const b = Math(Math.random() * 256)
-    return `rgb(${r}, ${b}, ${b})` 
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+    return `rgb(${r}, ${b}, ${b})`; 
 }
 
 const letters = document.querySelectorAll('.letter');
 const intervalId = setInterval(function(){
     for (let letter of letters)
-    {
-        letter.computedStyleMap.color = randomRGB();
+    { 
+        letter.style.color = randomRGB();
     }
 }, 1000);
