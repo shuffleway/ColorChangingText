89aa5c4780eb8893e144bd98a55284d79d3b7612
cngwane <cngwane@CollinsBitsPC>
2024-01-24 05:29:51 -0500

    add basic styles

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function randomRGB () {
-    const r = Math(Math.random() * 256)
-    const g = Math(Math.random() * 256)
-    const b = Math(Math.random() * 256)
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
     return `rgb(${r}, ${b}, ${b})` 
 }
 
@@ -9,6 +9,6 @@ const letters = document.querySelectorAll('.letter');
 const intervalId = setInterval(function(){
     for (let letter of letters)
     {
-        letter.computedStyleMap.color = randomRGB();
+        letter.style.color = randomRGB();
     }
 }, 1000);
